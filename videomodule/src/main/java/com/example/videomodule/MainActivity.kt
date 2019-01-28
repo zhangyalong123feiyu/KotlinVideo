93f60d763026826827639b010e03fd5b01af8919
zhangyalong <409812937@qq.com>
2019-01-28 16:30:17 +0800

    组件化测试

--- videomodule/src/main/java/com/example/videomodule/MainActivity.kt
+++ videomodule/src/main/java/com/example/videomodule/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.videomodule
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+* 调试launcher页面
+* */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
