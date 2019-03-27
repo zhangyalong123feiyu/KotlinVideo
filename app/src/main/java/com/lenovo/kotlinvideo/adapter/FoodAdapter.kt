50921abdfeeb8aed09873d4b7892534e49e8bb16
zhangyalong <409812937@qq.com>
2019-03-27 16:24:45 +0800

    Improve project compilation speed

--- app/src/main/java/com/lenovo/kotlinvideo/adapter/FoodAdapter.kt
+++ app/src/main/java/com/lenovo/kotlinvideo/adapter/FoodAdapter.kt
@@ -13,28 +13,28 @@ import com.lenovo.kotlinvideo.bean.Food
 import com.lenovo.kotlinvideo.ui.activity.ClassifyActivity
 import com.lenovo.kotlinvideo.utils.GlideApp
 
-class FoodAdapter(context: Context,foodData: Food) : RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
-    private  var context: Context
-    private var foodData:Food
+class FoodAdapter(context: Context, foodData: Food) : RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
+    private var context: Context
+    private var foodData: Food
+
     init {
-        this.context=context
-        this.foodData=foodData
+        this.context = context
+        this.foodData = foodData
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
-        val view= LayoutInflater.from(context).inflate(R.layout.item_food,null);
+        val view = LayoutInflater.from(context).inflate(R.layout.item_food, null);
         return ViewHolder(view);
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
-
         GlideApp.with(context).load(foodData.result!!.data!![i].albums!!.get(0)).into(viewHolder.imagePic!!)
-        viewHolder.textName!!.text= foodData.result!!.data!!.get(i).title
-        viewHolder.imagePic!!.setOnClickListener (object :View.OnClickListener{
+        viewHolder.textName!!.text = foodData.result!!.data!!.get(i).title
+        viewHolder.imagePic!!.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                var intent=Intent(context,ClassifyActivity::class.java)
-                intent.putExtra("parentId",foodData.result!!.data!!.get(i).id)
-                intent.putExtra("title",foodData.result!!.data!!.get(i).title)
+                var intent = Intent(context, ClassifyActivity::class.java)
+                intent.putExtra("parentId", foodData.result!!.data!!.get(i).id)
+                intent.putExtra("title", foodData.result!!.data!!.get(i).title)
                 context.startActivity(intent)
             }
         })
@@ -44,13 +44,15 @@ class FoodAdapter(context: Context,foodData: Food) : RecyclerView.Adapter<FoodAd
         return foodData.result!!.data!!.size
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-         var imagePic: ImageView ?=null
-         var textName: TextView ?=null
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var imagePic: ImageView? = null
+        var textName: TextView? = null
+
         init {
-            imagePic=itemView.findViewById(R.id.food_pic)
-            textName=itemView.findViewById(R.id.food_name)
+            imagePic = itemView.findViewById(R.id.food_pic)
+            textName = itemView.findViewById(R.id.food_name)
         }
 
     }
+
 }
--- app/src/main/java/com/lenovo/kotlinvideo/ui/activity/MainActivity.kt
+++ app/src/main/java/com/lenovo/kotlinvideo/ui/activity/MainActivity.kt
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import com.example.videomodule.IMyAidlInterface
 import com.example.videomodule.RemoteService
 import com.example.videomodule.SurfcacePlayer
@@ -18,13 +19,12 @@ import com.lenovo.kotlinvideo.adapter.FoodAdapter
 import com.lenovo.kotlinvideo.bean.Food
 import com.lenovo.kotlinvideo.mvp.view.FoodView
 import com.lenovo.kotlinvideo.ui.base.BaseActivity
-import com.example.videomodule.VideoActivity
 import com.lenovo.kotlinvideo.mvp.presenter.FoodPresenterLife
-
 
 class MainActivity : BaseActivity(), FoodView {
     private lateinit var reclerview: RecyclerView
     private var TAG: String = MainActivity::class.java.simpleName
+    private var adapter:FoodAdapter?=null
     private val serviceConnection= object : ServiceConnection {
         override fun onServiceDisconnected(name: ComponentName?) {
 
@@ -43,6 +43,7 @@ class MainActivity : BaseActivity(), FoodView {
 
     override fun initView() {
         reclerview = findViewById(R.id.recyclerView)
+
         reclerview.layoutManager = GridLayoutManager(this, 2)
         Log.i(TAG, "加载")
         findViewById<ImageView>(R.id.headerView).setOnClickListener(object : View.OnClickListener {
@@ -55,7 +56,9 @@ class MainActivity : BaseActivity(), FoodView {
 //                ARouter.getInstance().build("/videomodule/activity_video").navigation()
             }
         })
+
     }
+
 
     private fun bindRemoteService() {
         val intent=Intent(this,RemoteService::class.java)
@@ -70,7 +73,7 @@ class MainActivity : BaseActivity(), FoodView {
 
     override fun onGetFoodMenuSuccess(foodBean: Food) {
         Log.i(TAG, "成功")
-        var adapter = FoodAdapter(this, foodBean)
+        adapter = FoodAdapter(this, foodBean)
         reclerview.adapter = adapter
     }
 
